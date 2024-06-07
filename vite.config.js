5bedf596ba4d5f935ef22f461bb175ecf5402d47
adam <dathoang9635@gmail.com>
2024-06-07 16:00:46 +0700

    add test.yml

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,12 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   test: {
-//     globals: true,
-//     environment: 'jsdom',
-//     setupFiles: './src/test/setup.js',
-//   },
-// });
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.js',
   },
-  build: {
-    outDir: 'dist',
-  },
-})
+});
